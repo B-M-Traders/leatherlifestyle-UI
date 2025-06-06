d064a2b7108fc488f74c5771190b3942671dd445
Ansari Afroz Ahmed <ansariafroz.py@gmail.com>
2025-06-06 20:46:19 +0530

    Removind backend connection

--- components/collection_category/ProductContainer.tsx
+++ components/collection_category/ProductContainer.tsx
@@ -4,7 +4,7 @@ import { featuredProducts } from "@/lib/Menudata";
 
 const ProductContainer = () => {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-2 lg:gap-3 gap-y-6">
+    <div className="grid  grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-2 lg:gap-3 gap-y-6">
       {featuredProducts.map((item, index) => (
         <React.Fragment key={index}>
           <MainProductCard item={item as any} />
