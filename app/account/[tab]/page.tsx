e1d2ca942736a151a76068b31eb9e89cf1d9a919
Ansari Afroz Ahmed <ansariafroz.py@gmail.com>
2025-07-01 22:25:11 +0530

    Refactor TabPage component to simplify props and remove unused imports and commented code

--- app/account/[tab]/page.tsx
+++ app/account/[tab]/page.tsx
@@ -3,15 +3,10 @@ import Measurements from "@/components/AccountComps/Measurements";
 import Orders from "@/components/AccountComps/Orders";
 import Profile from "@/components/AccountComps/Profile";
 import Reviews from "@/components/AccountComps/Reviews";
-import CustomDivider from "@/components/ui/custom-divider";
-import { ChevronRight } from "lucide-react";
-import Link from "next/link";
 import React from "react";
 
 interface TabPageProps {
-  params: {
-    tab: string;
-  };
+  params: Promise<{ tab: string }>;
 }
 
 const TabPage = async ({ params }: TabPageProps) => {
@@ -19,23 +14,6 @@ const TabPage = async ({ params }: TabPageProps) => {
 
   return (
     <div className="space-y-5">
-      {/* <ul className="flex items-center font-light gap-1 text-xs">
-        <li>
-          <Link href="/" className="hover:underline underline-offset-2">
-            Home
-          </Link>
-        </li>
-        <li>
-          <ChevronRight size={18} strokeWidth={1} />
-        </li>
-        <li>
-          <span className="hover:underline underline-offset-2">Account</span>
-        </li>
-        <li>
-          <ChevronRight size={18} strokeWidth={1} />
-        </li>
-        <li className="underline underline-offset-2 capitalize">{tabLabel}</li>
-      </ul> */}
       <div>
         {tabLabel === "profile" && <Profile />}
         {tabLabel === "address" && <Address />}
